fc390cc8935e91b6914eb2fceaedf18af494d574
Francis <francis.tholley@scranton.edu>
2020-05-07 23:15:46 -0400

    work

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -5,11 +5,6 @@ public class MortgageCalculator {
     private static final byte MONTHS_IN_YEAR = 12;
     private static final byte PERCENT = 100;
 
-    private static float monthlyInterest;
-    private static short numberOfPayments;
-
-    private static int numberOfPaymentsMade = 1;
-
     public static void main(String[] args) {
         final int principalLowerLimit = 1000;
         final int principalUpperLimit = 1_000_000;
@@ -21,26 +16,29 @@ public class MortgageCalculator {
         float annualInterest = (float) readNumber("Annual Interest: ", lowerLimiter, upperLimiter);
         byte years = (byte) readNumber("Period (Years): ", lowerLimiter, upperLimiter);
 
-        monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        printMortgage(principal, annualInterest, years);
 
+        printPaymentSchedule(principal, annualInterest, years);
+
+    }
+
+    private static void printMortgage(int principal, float annualInterest, byte years) {
         // returned value of mortgage assigned to mortgage variable
         double mortgage = calculateMortgage(principal, annualInterest, years);
-
         // formart mortgage to currency
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("\nMORTGAGE\n--------\nMonthly Payments: " + mortgageFormatted + "\n");
+    }
 
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
         System.out.println("PAYMENT SCHEDULE\n----------------");
 
-        while (numberOfPaymentsMade <= numberOfPayments) {
-            double balance = calculateRemainingMortgage(principal, annualInterest, years);
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
+            double balance = calculateRemainingMortgage(principal, annualInterest, years, month);
 
-            // formart balance mortgage to currency
-            String balanceMortgageFormatted = NumberFormat.getCurrencyInstance().format(balance);
-            System.out.println(balanceMortgageFormatted);
+            // formart balance mortgage to currency and print to console
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
-
     }
 
     // reads in prompt
@@ -61,15 +59,21 @@ public class MortgageCalculator {
     // Calculate mortgage
     public static double calculateMortgage(int principal, float annualInterest, byte years) {
 
-        // calculates the mortgage 
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        // calculates the mortgage
         double mortgage = ((principal) * ((monthlyInterest * Math.pow((1 + monthlyInterest), (numberOfPayments))))
                 / ((Math.pow((1 + monthlyInterest), (numberOfPayments))) - 1));
 
         return mortgage;
     }
 
-    public static double calculateRemainingMortgage(int principal, float annualInterest, byte years) {
+    // Calculate mortgage balance
+    public static double calculateRemainingMortgage(int principal, float annualInterest, byte years,
+            short numberOfPaymentsMade) {
 
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = (short) (years * MONTHS_IN_YEAR);
         // calculates the mortgage balance
         double balance = ((principal) * (Math.pow((1 + monthlyInterest), numberOfPayments)
                 - Math.pow((1 + monthlyInterest), numberOfPaymentsMade)))
